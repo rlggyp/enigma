8eae8152975da144035064f840f02b6fab8ef680
Erlangga Yudi Pradana <erlanggayudipradana@gmail.com>
2025-01-02 09:25:38 +0700

    add rotation rotor offset

--- main.c
+++ main.c
@@ -15,14 +15,16 @@ void UpperCase(char *input, const size_t *len) {
   }
 }
 
-void Encrypt(char *right, char *left) {
-  *right = left[*right - 65];
+void Encrypt(char *right, const char *left, const char offset) {
+  char i = ((*right - 'A') + offset) % 26;
+  
+  *right = left[i];
 }
 
-void Backward(char *right, char *left) {
+void Backward(char *right, const char *left, const char offset) {
   for (short i = 0; i < 26; ++i) {
-    if (*right == left[i]) {
-      *right = 65 + i;
+    if (*right == left[(i + offset) % 26]) {
+      *right = 'A' + i;
       break;
     }
   }
@@ -46,17 +48,30 @@ int main() {
 
   char *input = NULL;
   size_t len;
+  printf("Enter text: ");
   getline(&input, &len, stdin);
-
   UpperCase(input, &len);
-  Encrypt(&input[0], rotor_3);
-  Encrypt(&input[0], rotor_2);
-  Encrypt(&input[0], rotor_1);
-  Backward(&input[0], rotor_1);
-  Backward(&input[0], rotor_2);
-  Backward(&input[0], rotor_3);
 
-  printf("%c\n", input[0]);
+  char offset = 0;
+
+  for (int i = 0; i < len; ++i) {
+    if (input[i] < 32)
+      break;
+    else if(input[i] == 32)
+      continue;
+
+    ++offset;
+    Encrypt(&input[i], rotor_3, offset);
+    Encrypt(&input[i], rotor_2, 0);
+    Encrypt(&input[i], rotor_1, 0);
+    Encrypt(&input[i], reflector, 0);
+    Backward(&input[i], rotor_1, 0);
+    Backward(&input[i], rotor_2, 0);
+    Backward(&input[i], rotor_3, offset);
+  }
+
+  printf("Encrypted: %s", input);
+  free(input);
 
   return 0;
 }
